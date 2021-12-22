1e3fdeb95d13dc895cceb416ae697f3bf37988f4
Eduardo Catalán <marvineduardocv12@gmail.com>
2021-12-21 18:59:15 -0600

    Update Node.js conection with oracle.

---
+++ Server/server.js
@@ -0,0 +1,74 @@
+
+var express = require("express");
+var app = express();
+var bodyparser = require('body-parser');
+var oracledb = require('oracledb');
+
+app.use(bodyparser.json());
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+ 
+var connAttrs = {
+    "user": "DBP2",
+    "password": "1234",
+    "connectString": "localhost:1521/ORCL18"
+    //"(DESCRIPTION =(LOAD_BALANCE = ON)(FAILOVER = ON)(ADDRESS =(PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)(FAILOVER_MODE=(TYPE=SELECT)(METHOD = BASIC))))"
+}
+
+app.get('/',(req,res)=>{
+    res.send([{message: 'hola nenes'}]);
+});
+
+
+app.get('/prueba', function (req, res) {
+    "use strict";
+
+    oracledb.getConnection(connAttrs, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to DB",
+                detailed_message: err.message
+            }));
+            return;
+        }
+        connection.execute("SELECT * FROM tipousuario", {}, {
+            outFormat: oracledb.OBJECT // Return the result as Object
+        }, function (err, result) {
+            if (err) {
+                res.set('Content-Type', 'application/json');
+                res.status(500).send(JSON.stringify({
+                    status: 500,
+                    message: "Error getting the dba_tablespaces",
+                    detailed_message: err.message
+                }));
+            } else {
+                res.header('Access-Control-Allow-Origin','*');
+                res.header('Access-Control-Allow-Headers','Content-Type');
+                res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify(result.rows));
+				
+            }
+            // Release the connection
+            connection.release(
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log("GET /sendTablespace : Connection released");
+                    }
+                });
+        });
+    });
+});
+
+app.listen(4015,'localhost',function(){
+    console.log("el servidor esta escuchando desde el puerto 4015 pa :D")
+})
+
+
