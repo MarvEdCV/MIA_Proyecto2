d2a7d3115ea34da9b9f3768733989192f1c2b793
Eduardo Catalán <marvineduardocv12@gmail.com>
2021-12-23 21:47:02 -0600

    Update conexion API con FRONTEND (login)
    Se agrega en el componente de login el metodo realizado en el servicio que consume la API de la base de datos.

--- Soccer_Statistics_web/src/app/Componentes/login/login.component.ts
+++ Soccer_Statistics_web/src/app/Componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Servicios/login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,22 @@ export class LoginComponent implements OnInit {
   email: string="";
   password: string="";
 
-  constructor() { }
+  constructor(public LoginService: LoginService,private router:Router) { }
+  login(){
+
+    
+    const user = {email: this.email, pwd: this.password};
+    this.LoginService.Login(user).subscribe((data)=>{
+        this.router.navigate(['/Registro']);
+        console.log(data);
+    },(err)=>{
+      if(err.status===403){
+        alert('Correo no confirmado!!')
+      }else if(err.status===401){
+        alert('Correo o contraseña invalida!!') 
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
