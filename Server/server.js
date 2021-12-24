5d89008277234abf59b915f08e83f5c3461f055f
Eduardo Catalán <marvineduardocv12@gmail.com>
2021-12-23 21:45:58 -0600

    Update server.js
    1.Se agregan los header para validar los cors en la utilizacion con el frontend en Angular.
    2.La configuración de la base de datso en el connectString se soluciona para que funcione de manera correcta en el puerto establecido.
    3.Se termina en la API del servidor el metodo de login para el ingreso de los usuarios.

--- Server/server.js
+++ Server/server.js
@@ -4,20 +4,30 @@ var app = express();
 var bodyparser = require('body-parser');
 var oracledb = require('oracledb');
 const bcrypt = require('bcrypt');
+const { sign } = require('jsonwebtoken')
 
 app.use(bodyparser.json());
 
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
  
+
 var connAttrs = {
     "user": "DBP2",
     "password": "1234",
     "connectString": "localhost:1521/ORCL18"
-    //"(DESCRIPTION =(LOAD_BALANCE = ON)(FAILOVER = ON)(ADDRESS =(PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)(FAILOVER_MODE=(TYPE=SELECT)(METHOD = BASIC))))"
 }
-//hola
+
 
 app.get('/',(req,res)=>{
     res.send([{message: 'hola nenes'}]);
@@ -27,8 +37,9 @@ app.post('/login', async (req, res) => {
     console.log(req.body);
     let body = req.body
     let result = await login(body)
+    console.log(result)
     if (result.ok) {
-        res.send('confirmado!!')
+        //res.send('confirmado!!')
         return res.status(200).send(result)
     } else {
         if (result.err) {
@@ -36,28 +47,11 @@ app.post('/login', async (req, res) => {
             return res.status(500).send(result)
         } else {
             // Usuario o contraseña inválidos
-            res.send('usuario o contra incoicas incorrectas')
+            //res.send('usuario o contra incoicas incorrectas')
             return res.status(result.status).send({ ok: false })
         }
     }
 })
-
-/*
-app.post('/aa/login',(req,res) =>{
-    //recibiremos el user y la pass en json
-
-
-
-    const {ID_TIPOUSUARIO,TIPO_USUARIO} = req.body;
-    if(ID_TIPOUSUARIO && TIPO_USUARIO){
-        res.send('datos correctos!');
-        console.log(req.body);
-    }else{
-        console.log(req.body);
-        res.send('falta un dato');
-    }   
-    
-})*/
 
 app.post('/registrar',(req,res) =>{
     const {usuario,contra,email,tipo_usuario,nombre,apellido} = req.body;
@@ -69,7 +63,7 @@ app.post('/registrar',(req,res) =>{
 async function login(req) {
     let con, result
     // consulta a ejecutar
-    const query = "SELECT usuarios.EMAIL, usuarios.CONTRA" +
+    const query = "SELECT usuarios.ID, usuarios.CONTRA,usuarios.tipousuario_id_tipousuario,usuarios.Confirmado" +
         " FROM DBP2.USUARIOS " +
         " WHERE usuarios.EMAIL = :email"
     // datos a insertar
@@ -90,20 +84,24 @@ async function login(req) {
         }
     }
     if (result.rows.length == 1) {
-        //let confirmado = result.rows[0][3]
-        let pwd = result.rows[0][1]
-            if (bcrypt.compareSync(req.pwd, pwd)) {
-                //let id = result.rows[0][0]
-                /*let esAdmin = result.rows[0][2]
+        let confirmado = result.rows[0][3]
+        let pwd = result.rows[0][1];
+        
+        if(confirmado==1){
+            console.log(confirmado);
+            if (pwd==req.pwd) {
+                let id = result.rows[0][0]
+                let esAdmin = result.rows[0][2]
                 let token = sign({ id, esAdmin },
                     'seed-de-dessarollo-miap2',
-                    { expiresIn: '48h' })*/
+                    { expiresIn: '48h' })
                 // Credenciales correctas y correo confirmado
-                return { ok: true/*, token, esAdmin*/ }
+                return { ok: true,token,esAdmin}
             }
-        
-    }else{
-        return { ok: false, status: 401 }   
+        }else{
+            // Usuario aún no está confirmado
+            return { ok: false, status: 403 }
+        }        
     }
     // Correo o contraseña incorrecta
     return { ok: false, status: 401 }
