8bc05129bbd829c4b53775ef8cccf4ed4645edf7
Eduardo Catalán <marvineduardocv12@gmail.com>
2021-12-30 16:56:20 -0600

    Update empleados
    Creacion de empleados y administradores

--- Soccer_Statistics_web/src/app/Componentes/empleados/empleados.component.ts
+++ Soccer_Statistics_web/src/app/Componentes/empleados/empleados.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Servicios/login.service';
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
@@ -11,9 +12,32 @@ export class EmpleadosComponent implements OnInit {
   Equipo:any;
 
 
-  constructor() { }
+  constructor(public LoginService: LoginService,private router:Router) { }
 
   ngOnInit(): void {
+    //AGREGAR PARA QUE SE CARGUEN LOS EQUIPOS YA REGISTRADOS
+  }
+
+  Noticia(){
+    if(this.noticia.trim().length===0){
+      alert('llene todos los campos por favor')
+      return
+    }
+    console.log(this.Equipo)
+    const noticia = {
+      noticia:this.noticia,
+      equipo: this.Equipo
+    }
+    this.LoginService.GenerarNoticia(noticia).subscribe((data)=>{
+      console.log(data)
+      alert('noticia generada con exito')
+    },(err)=>{
+      if(err.status===500){
+        alert(
+          'El equipo no existe'
+        )
+      }
+    })
   }
 
 }
--- Soccer_Statistics_web/src/app/Componentes/reg-adm-emp/reg-adm-emp.component.ts
+++ Soccer_Statistics_web/src/app/Componentes/reg-adm-emp/reg-adm-emp.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Servicios/login.service';
 @Component({
   selector: 'app-reg-adm-emp',
   templateUrl: './reg-adm-emp.component.html',
@@ -16,14 +17,63 @@ export class RegAdmEmpComponent implements OnInit {
   FechaN: any;
   FechaR: any;
   Direccion: any;
-  membresia: any;
-  pais: any;
-  noticias: any;
+  pais:any;
+
+  Eemail: any;
+  Epassword: any;
+  Enombre: any;
+  Eapellido: any;
 
 
-  constructor() { }
+  constructor(public LoginService: LoginService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  RegistrarAdmin(){
+    const user ={nombre: this.nombre,apellido: this.apellido,pais:this.pais,fecha:this.FechaN,email:this.email,pwd:this.password,foto:this.foto,direccion:this.Direccion,telefono:this.telefono,genero:this.genero};
+    if (this.nombre.trim().length === 0||
+      this.apellido.trim().length === 0 ||
+      this.pais.trim().length === 0 ||
+      this.FechaN.trim().length === 0 ||
+      this.email.trim().length === 0 ||
+      this.password.trim().length === 0 ||
+      this.foto.trim().length === 0 ||
+      this.Direccion.trim().length === 0 ||
+      this.telefono.trim().length === 0 ||
+      this.genero.trim().length ===0){ 
+        alert('Alguno de los campos se encuentra vacio favor de llenarlo')
+      return
+      }
+
+    this.LoginService.RegistroAdmin(user).subscribe((data)=>{
+      alert('Administrador Creado con éxito!!')
+      this.router.navigate(['/login'])
+    
+      
+    },(err)=>{
+      if(err.status===422){
+        alert('Este correo ya esta registrado!!')
+      }else{
+        alert('Error pendiente')
+      }
+    })
+  }
+
+  RegistrarEmpleado(){
+    const user = {nombre:this.Enombre,apellido:this.Eapellido,email:this.Eemail,pwd:this.Epassword}
+    this.LoginService.RegistroEmpleado(user).subscribe((data)=>{
+      alert('Empleado Creado con éxito!!')
+      this.router.navigate(['/login'])
+    
+      
+    },(err)=>{
+      if(err.status===422){
+        alert('Este correo ya esta registrado!!')
+      }else{
+        alert('Error pendiente')
+      }
+    })
   }
 
 }
