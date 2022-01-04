6d6de7156f0ee2a31a1f4d3076fcaf89317a20f6
Sara Paulina Medrano Cojulún <71093494+spmc1605@users.noreply.github.com>
2022-01-03 22:39:21 -0600

    Reportes clientes
    se llenaron las tablas de los reportes de los clientes con lo json obtenidos de las consultas realizadas en la Api

--- Soccer_Statistics_web/src/app/Componentes/repo-clientes/repo-clientes.component.ts
+++ Soccer_Statistics_web/src/app/Componentes/repo-clientes/repo-clientes.component.ts
@@ -23,6 +23,15 @@ export class RepoCLientesComponent implements OnInit {
   ListaEquipos:string[]=[];
   ListaPaises:string[]=[];
   ListaCompeticiones:string[]=[]; 
+  jsonR4:any;
+  jsonR5:any;
+  jsonR6:any;
+  jsonR7:any;
+  jsonR8:any;
+  jsonR9:any;
+  jsonR17:any;
+  jsonR15:any;
+  jsonR12:any;
   constructor(public LoginService: LoginService,private router:Router) { }
 
   ngOnInit(): void {
@@ -55,6 +64,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={nombre:this.selecCompeticion}
     this.LoginService.Reporte4(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR4=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -62,6 +72,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={nombre:this.selecPais}
     this.LoginService.Reporte5(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR5=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -69,6 +80,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={numero:this.anios}
     this.LoginService.Reporte6(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR6=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -76,6 +88,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={nombre:this.selecPaisE}
     this.LoginService.Reporte7(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR7=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -83,6 +96,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={numero:this.capacidad}
     this.LoginService.Reporte8(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR8=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -90,6 +104,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={nombre:this.selecEquipo1}
     this.LoginService.Reporte9Y17(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR9=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -97,6 +112,7 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={nombre:this.selecEquipo2}
     this.LoginService.Reporte9Y17(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR17=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
@@ -104,12 +120,14 @@ export class RepoCLientesComponent implements OnInit {
     const competi ={numero:this.anio}
     this.LoginService.Reporte15(competi).subscribe((data)=>{
       console.log(data);
+      this.jsonR15=JSON.parse(data);
       //EN DATA ESTA EL JSON CON LA CONSULTA
     })
   }
   Reporte12(){
     this.LoginService.Reporte12().subscribe((data)=>{
       console.log(data);
+      this.jsonR12=JSON.parse(data);
     })
   }
 }
